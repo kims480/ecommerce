9fc6281f01f44994e3d2de3ee04aaa4fc0e474df
KarimSaleh <eng.karim2010@gmail.com>
2023-07-08 05:29:07 +0400

    ecommerce_5

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,7 +8,7 @@
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <meta http-equiv="x-ua-compatible" content="ie=edge">
-      <title>@yield('title')</title>
+      <title>Rahaal Store @yield('title')</title>
 
       <!-- SEO -->
       <meta name="description" content="منتجات مبتكرة ومنتجات ابداعية">
